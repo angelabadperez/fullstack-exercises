9188c46765f37960fb4e638f975044ed05f3cdf3
Ángel Abad <angelabadperez@gmail.com>
2025-07-01 00:05:24 +0200

    feat: Add DELETE and PUT methods to blogs

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -6,14 +6,32 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
-  try {
-    const blog = new Blog(request.body)
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  } catch (exception) {
-    next(exception)
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end() 
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const newBlog = request.body
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
   }
+
+  blog.title = newBlog.title
+  blog.author = newBlog.author
+  blog.url = newBlog.url
+  blog.likes = newBlog.likes
+
+  const updatedBlog = await blog.save()
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
