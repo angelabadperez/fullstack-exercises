8eaf259e7b29d75cb5b2d398067f5729fab9b915
Ángel Abad <angelabadperez@gmail.com>
2025-08-05 23:49:48 +0200

    feat: First commit for the anecdotes redux app

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+Anecdote.propTypes = {
+  anecdote: PropTypes.object.isRequired,
+  handleClick: PropTypes.func.isRequired
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
+
+  return (
+    <div>
+      {anecdotes.map(anecdote => (
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => dispatch(vote(anecdote.id))}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
