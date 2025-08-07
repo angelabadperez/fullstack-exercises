a7d9455e1781c20c3922afdd30adc1571f74a18b
Ángel Abad <angelabadperez@gmail.com>
2025-08-07 14:30:26 +0200

    feat: Get anecdotes from DB

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Notification from './components/Notification'
 import AnecdoteFilter from './components/AnecdoteFilter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [])
+
   return (
     <div>
       <Notification />
